4fe1d4a0e23d48d6f44679a95ec7dad457850583
damniko <ndam20@student.aau.dk>
2023-04-05 10:27:45 +0200

    Added early exit functionality, test loop

--- ml-models/main.py
+++ ml-models/main.py
@@ -1,8 +1,9 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import torch
 
 from gru.gru import GRUNet
-from training import train
+from training import train, test
 from mlp.mlp import MLP
 from data_utils import get_processed_data, prepare_X_and_y, flatten_X_for_MLP
 
@@ -10,17 +11,30 @@ if __name__ == '__main__':
     seq_length = 12         # Number of time-steps to use for each prediction
     target_length = 12      # Number of time-steps to predict
     target_col = 'temp'     # The column to predict
+    batch_size = 32
 
+    model = GRUNet(input_size=32, hidden_size=32, output_size=1, dropout_prob=0, num_layers=1)
+
+    # Prepare data
     df = get_processed_data('./data/open-weather-aalborg-2000-2022.csv')
+    train_df, test_df = train_test_split(df, train_size=0.6, shuffle=False)
 
-    train_df, test_df = train_test_split(df, train_size=0.7, shuffle=False)
-
+    # Train
     X_train, y_train = prepare_X_and_y(train_df, n_steps_in=seq_length, n_steps_out=target_length, target_column=target_col)
-
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train.reshape(-1, X_train.shape[-1])).reshape(X_train.shape)
+    try:
+        train(model, X_train, y_train, batch_size, './saved-models/gru01.pt')
+    except KeyboardInterrupt:
+        print("Exiting early from training")
+        state_dict = torch.load('./saved-models/gru01.pt')
+        model.load_state_dict(state_dict)
+        model.eval()
 
-    model = GRUNet(input_size=32, hidden_size=128, output_size=1, dropout_prob=0.2, num_layers=2)
+    print("\n====================\n")
 
-    train(model, X_train, y_train)
+    # Test
+    X_test, y_test = prepare_X_and_y(test_df, n_steps_in=seq_length, n_steps_out=target_length, target_column=target_col)
+    X_test = scaler.transform(X_test.reshape(-1, X_test.shape[-1])).reshape(X_test.shape)
+    test(model, X_test, y_test, batch_size)
 
--- ml-models/training.py
+++ ml-models/training.py
@@ -8,10 +8,9 @@ from tqdm import tqdm
 from datasets import RegressionDataset
 
 
-def train(model, X_train, y_train, save_path=None):
+def train(model, X_train, y_train, batch_size, save_path=None):
     train_dataset = RegressionDataset(X_train, y_train)
-    BATCH_SIZE = 32
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
     # Define training parameters
     torch.manual_seed(0)
@@ -22,11 +21,11 @@ def train(model, X_train, y_train, save_path=None):
 
     best_loss = np.Infinity
 
+    print('Begin training')
     # training loop
     for epoch in range(epochs):
         model.train()
         curr_loss = 0.0
-
         for batch, (X_batch, y_batch) in enumerate(tqdm(train_loader, desc=f'Epoch {epoch + 1} of {epochs}')):
             optimizer.zero_grad()
 
@@ -38,9 +37,25 @@ def train(model, X_train, y_train, save_path=None):
 
             curr_loss += loss.item()
 
+        print(f"- avg_loss = {(curr_loss / len(train_loader)):>.3f}")
         if curr_loss < best_loss and save_path is not None:
-            torch.save(model, save_path)
+            torch.save(model.state_dict(), save_path)
 
-        print(
-            f"\t- avg_loss = {(curr_loss / (len(train_dataset) / BATCH_SIZE)):>4.4f}")
-        curr_loss = 0.0
+    print(f'End of training\n- best_loss = {best_loss}')
+
+
+def test(model, X_test, y_test, batch_size):
+    dataset = RegressionDataset(X_test, y_test)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    loss_fn = nn.L1Loss()
+
+    total_loss = 0.0
+    print('Begin testing')
+    model.eval()
+    with torch.no_grad():
+        for batch, (X_batch, y_batch) in enumerate(tqdm(dataloader, desc=f'Testing')):
+            y_pred = model(X_batch)
+            loss = loss_fn(y_pred, y_batch)
+            total_loss += loss.item()
+
+    print(f"End of testing\n- loss = {(total_loss / len(dataloader)):>.3f}")
