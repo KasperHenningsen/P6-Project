a37a9f8d5ed4a789121b3683f3912dd3a16f8b1a
AndersMariegaard <74593851+AndersMariegaard@users.noreply.github.com>
2023-04-16 01:14:17 +0200

    Added matrix search, cell highlighting and refactored feature matrix JS and CSS

--- WebInterface/app/javascript/controllers/heatmap/featurematrix_controller.js
+++ WebInterface/app/javascript/controllers/heatmap/featurematrix_controller.js
@@ -4,46 +4,61 @@ import * as d3 from "d3-scale";
 // Connects to data-controller="heatmap--featurematrix"
 
 export default class extends Controller {
+    matrixData;
+    features;
+
     connect() {
-        this.buildMatrix()
+        this.initializeData();
+        this.buildMatrix();
+    }
+
+    initializeData() {
+        this.matrixData = JSON.parse(this.data.get("features"));
+
+        this.features = this.matrixData[0];
+        this.features[0] = "";
     }
 
     buildMatrix() {
-        let matrixData = JSON.parse(this.data.get("features"));
-        let features = matrixData[0];
-        features[0] = "";
-
         const cellColor = d3.scaleLinear().domain([1, -1]).range(["red", "blue"]);
 
         const GRID = document.querySelector("#matrix");
-        GRID.style.gridTemplateColumns = `max-content repeat(${features.length}, minmax(12px, 2.5vh))`;
+        GRID.style.gridTemplateColumns = `max-content repeat(${this.features.length}, minmax(12px, 2.5vh))`;
 
-        const ROWS = features.length;
+        const ROWS = this.features.length;
         for (let i = 1; i <= ROWS; i++) {
             for (let j = 0; j < ROWS; j++) {
                 const cell = document.createElement("div");
-                cell.style.gridArea = `${i + 1} / ${j + 1}`;
-
+                let id = i + "/" + (j + 1);
+                cell.setAttribute("id", `${id}`)
+                cell.style.gridArea = `${i} / ${j + 1}`;
                 let content = "";
+
                 if (i === ROWS) { //bottom label
                     if (j > 0) {
-                        content = features[j];
-                        cell.classList.add("matrix-label", "matrix-label-bottom");
-                        cell.innerText += content;
-                    } else cell.classList.add("matrix-cell")
+                        const label = document.createElement("div");
+                        content = this.features[j];
+                        label.classList.add("label", "bottom");
+                        label.innerText += content;
+                        cell.classList.add("container-bottom");
+                        cell.appendChild(label);
+                    } else cell.classList.add("matrix-cell") // Corner cell
                 } else if (j === 0) { //left label
-                    content = matrixData[i][j];
-                    cell.classList.add("matrix-label");
+                    content = this.matrixData[i][j];
+                    cell.classList.add("label");
                     cell.innerText += content;
                 }
                 else if (j < i) { //data cells
-                    //content = parseFloat(matrixData[i][j]).toFixed(3); //Correlation coefficient
-                    cell.style.backgroundColor = cellColor(matrixData[i][j]);
+                    cell.style.backgroundColor = cellColor(this.matrixData[i][j]);
                     cell.classList.add("matrix-cell", "data-cell");
 
+                    let controller = "heatmap--featurematrix"
+                    cell.setAttribute("data-action", `mouseover->${controller}#addHoverBorder mouseout->${controller}#removeHoverBorder`);
+
+                    let content = parseFloat(this.matrixData[i][j]).toPrecision(3);
                     const tooltip = document.createElement("div");
-                    tooltip.classList.add("tooltiptext")
-                    tooltip.innerText += `${features[j] + " / "  + matrixData[i][0] + " \n " + parseFloat(matrixData[i][j]).toPrecision(3)}`
+                    tooltip.classList.add("tooltiptext");
+                    tooltip.innerText += `${this.features[j] + " / "  + this.matrixData[i][0] + " \n " + content}`
                     cell.appendChild(tooltip)
                 }
                 else {
@@ -52,6 +67,48 @@ export default class extends Controller {
                 GRID.appendChild(cell);
             }
         }
-        if (matrixData.length !== 0) document.querySelector("#legend").style.visibility = "visible";
+        if (this.matrixData.length !== 0) document.querySelector("#legend").style.visibility = "visible";
+    }
+
+    searchMatrix(event) {
+        if (event.currentTarget.value) {
+            this.clearLowlights();
+            const ROWS = this.features.length;
+            for (let i = 1; i < ROWS; i++) {
+                for (let j = 0; j < i; j++) {
+                    if (Number(this.matrixData[i][j]) < event.currentTarget.value) {
+                        let id = i + "/" + (j + 1);
+                        let TARGET = document.querySelector("#" + CSS.escape(id));
+                        if (TARGET) TARGET.classList.add("lowlight");
+                    }
+                }
+            }
+        }
+    }
+
+    clearLowlights() {
+        const TARGETS = document.querySelectorAll(".lowlight");
+        TARGETS.forEach(elem => elem.classList.remove("lowlight"));
+    }
+
+    addHoverBorder(event) {
+        let gridPos = event.currentTarget.id.split('/');
+        let row = gridPos[0];
+        let col = gridPos[1];
+
+        for (let i = row; i <= this.features.length; i++) {
+            let cell = `${i}/${gridPos[1]}`
+            document.querySelector("#" + CSS.escape(cell)).classList.add("border-ver")
+        }
+
+        for (let i = col; i > 0; i--) {
+            let cell = `${gridPos[0]}/${i}`
+            document.querySelector("#" + CSS.escape(cell)).classList.add("border-hor")
+        }
+    }
+
+    removeHoverBorder() {
+        let borderCells = document.querySelectorAll(".border-ver, .border-hor")
+        borderCells.forEach(cell => cell.classList.remove("border-ver", "border-hor"))
     }
 }
