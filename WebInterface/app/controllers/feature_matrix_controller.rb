d29c37a20b2394f51dd9299c69bb234f4d8ea035
AndersMariegaard <74593851+AndersMariegaard@users.noreply.github.com>
2023-04-17 14:31:18 +0200

    Matrix data is now requested from an api, model for featurematrix and local csv have been removed

--- WebInterface/app/controllers/feature_matrix_controller.rb
+++ WebInterface/app/controllers/feature_matrix_controller.rb
@@ -1,5 +1,16 @@
 class FeatureMatrixController < ApplicationController
   def index
-    @features = FeatureMatrix.index
+    @features = getData
+  end
+
+  def getData
+    require 'json'
+    require 'uri'
+    require 'net/http'
+    
+    uri = URI('http://localhost:5000/featurematrix')
+    res = Net::HTTP.get_response(uri)
+    data = res.body if res.is_a?(Net::HTTPSuccess)
+    CSV.parse(data).to_json
   end
 end
