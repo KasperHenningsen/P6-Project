08db9ccdc8705372832726e33cc09f4a0a99b445
damniko <ndam20@student.aau.dk>
2023-04-22 16:27:00 +0200

    fix hardcoded output sequence length for GRU, RNN

--- ml-models/baselines/rnn.py
+++ ml-models/baselines/rnn.py
@@ -27,7 +27,7 @@ class RNN(nn.Module):
         h0 = self.init_internal_states()
         out, hn = self.rnn(x, h0.detach())
         out = self.linear(out)
-        return out.reshape(-1, 12)
+        return torch.squeeze(out)
 
     def init_internal_states(self):
         h0 = torch.zeros(1 * self.num_layers, self.batch_size, self.hidden_size, dtype=torch.float64).to(settings.device)
